75c7bf23fabb26c1d3ea49af3eff9cdad418f7bc
danielp67 <daniel.67100@hotmail.fr>
2024-03-17 20:09:09 +0100

    add second firebase function on update message

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -22,12 +22,12 @@ import * as admin from "firebase-admin";
 admin.initializeApp();
 
 export const onConversationsCreated = functions.firestore.
-  document("Conversations/{conversationID}").
-  onCreate((snapshot: functions.firestore.QueryDocumentSnapshot,
+  document("Conversations/{conversationID}")
+  .onCreate((snapshot: functions.firestore.QueryDocumentSnapshot,
     context: functions.EventContext<{ conversationID: string; }>) => {
     const data = snapshot.data();
     const conversationID = context.params.conversationID;
-
+    console.log(data, context.params);
     if (data) {
       const members = data.members;
       for (let index = 0; index < members.length; index++) {
@@ -42,7 +42,7 @@ export const onConversationsCreated = functions.firestore.
                 return admin.firestore().collection("Users")
                   .doc(currentUserID).collection("Conversations")
                   .doc(m).create({
-                    "chatID": conversationID,
+                    "conversationID": conversationID,
                     "image": userData.image,
                     "name": userData.name,
                     "unseenCount": 1,
@@ -51,6 +51,35 @@ export const onConversationsCreated = functions.firestore.
               return null;
             }).catch(() => {
               return null;
+            });
+        });
+      }
+    }
+    return null;
+  });
+
+
+export const onConversationsUpdated = functions.firestore
+  .document("Conversations/{conversationID}")
+  .onUpdate((change: functions
+    .Change<functions.firestore.QueryDocumentSnapshot>,
+  context: functions.EventContext<{conversationID: string;}>) => {
+    const data = change?.after.data();
+    if (data) {
+      console.log(data);
+      const members = data.members;
+      const lastMessage = data.messages[data.messages.length -1];
+      for (let index = 0; index < members.length; index++) {
+        const currentUserID = members[index];
+        const remainingUserIDs = members
+          .filter((u: string) => u != currentUserID);
+        remainingUserIDs.forEach((u: string) => {
+          return admin.firestore().collection("Users")
+            .doc(currentUserID).collection("Conversations")
+            .doc(u).update({
+              "lastMessage": lastMessage.message,
+              "timestamp": lastMessage.timestamp,
+              "unseenCount": admin.firestore.FieldValue.increment(1),
             });
         });
       }
