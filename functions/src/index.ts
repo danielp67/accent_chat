eecd794ae49a809bfbb67701745ab4d677371613
danielp67 <daniel.67100@hotmail.fr>
2024-03-17 16:56:38 +0100

    create firestore function 1

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -7,8 +7,9 @@
  * See a full list of supported triggers at https://firebase.google.com/docs/functions
  */
 
-import {onRequest} from "firebase-functions/v2/https";
-import * as logger from "firebase-functions/logger";
+
+import * as functions from "firebase-functions";
+import * as admin from "firebase-admin";
 
 // Start writing functions
 // https://firebase.google.com/docs/functions/typescript
@@ -17,3 +18,42 @@ import * as logger from "firebase-functions/logger";
 //   logger.info("Hello logs!", {structuredData: true});
 //   response.send("Hello from Firebase!");
 // });
+
+admin.initializeApp();
+
+export const onConversationsCreated = functions.firestore.
+  document("Conversations/{conversationID}").
+  onCreate((snapshot: functions.firestore.QueryDocumentSnapshot,
+    context: functions.EventContext<{ conversationID: string; }>) => {
+    const data = snapshot.data();
+    const conversationID = context.params.conversationID;
+
+    if (data) {
+      const members = data.members;
+      for (let index = 0; index < members.length; index++) {
+        const currentUserID = members[index];
+        const remainingUserIDs = members.
+          filter((u: string) => u !== currentUserID);
+        remainingUserIDs.forEach((m: string) => {
+          return admin.firestore().collection("Users")
+            .doc(m).get().then((_doc) => {
+              const userData = _doc.data();
+              if (userData) {
+                return admin.firestore().collection("Users")
+                  .doc(currentUserID).collection("Conversations")
+                  .doc(m).create({
+                    "chatID": conversationID,
+                    "image": userData.image,
+                    "name": userData.name,
+                    "unseenCount": 1,
+                  });
+              }
+              return null;
+            }).catch(() => {
+              return null;
+            });
+        });
+      }
+    }
+    return null;
+  });
